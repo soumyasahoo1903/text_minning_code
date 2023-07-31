7d6a1ce0b9f35dee6beb4e1a2a383f8b8c6cd979
soumya sahoo <135194013+soumyasahoo1903@users.noreply.github.com>
2023-07-31 13:48:50 +0530

    Update 2) merge all the excel file in one .pydirectory.py

--- 2) merge all the excel file in one .pydirectory.py
+++ 2) merge all the excel file in one .pydirectory.py
@@ -2,29 +2,27 @@ import os
 import pandas as pd
 
 # Directory path containing the Excel files
-directory_path = '' ### give the directory path copy it form the folder 
+directory_path = 'F:/NISER internship/text-mining_common_ metabolites/New folder' ## folder path 
 
 # Initialize an empty list to store data from all Excel files
-all_metabolites = []
+all_data = []
 
 # Loop through all files in the directory
 for filename in os.listdir(directory_path):
     if filename.endswith('.xlsx'):  # Assuming the files have a .xlsx extension
         file_path = os.path.join(directory_path, filename)
-        # Read the Excel file and extract the metabolites column into a DataFrame
+        # Read the Excel file and append it to the list
         df = pd.read_excel(file_path)
-        metabolites_column = df['Metabolites'] ### give the column name here which is common for all the files 
-        # Append the metabolites from the current file to the list
-        all_metabolites.append(metabolites_column)
+        all_data.append(df)
 
-# Concatenate all metabolite columns into one DataFrame
-merged_metabolites_df = pd.concat(all_metabolites, ignore_index=True)
+# Concatenate all DataFrames into one DataFrame
+merged_data_df = pd.concat(all_data, ignore_index=True)
 
 # Optionally, you can remove duplicates from the merged DataFrame
-merged_metabolites_df.drop_duplicates(inplace=True)
+merged_data_df.drop_duplicates(inplace=True)
 
 # Save the merged DataFrame to a new Excel file
-output_file_path = '--' #### give the output file path here 
-merged_metabolites_df.to_excel(output_file_path, index=False)
+output_file_path = 'F:/NISER internship/text-mining_common_ metabolites/New folder/output_file.xlsx' ## give output excel file path where all the merged data will be saved 
+merged_data_df.to_excel(output_file_path, index=False)
 
 print(f"Merged data saved to {output_file_path}")
